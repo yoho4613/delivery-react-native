5f33728d00becfa248c29fe22964a746d690d5a2
박지호 <yoho4613@gmail.com>
2023-06-15 01:35:30 +1200

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <Text className=" text-red-500">Home Screen</Text>
+  )
+}
+
+export default HomeScreen
